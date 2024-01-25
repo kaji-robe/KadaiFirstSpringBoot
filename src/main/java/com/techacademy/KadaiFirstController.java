1b9b7538a06276c2bf99b5dd5093e0520d4d1cc2
kaji-robe <hayano3.141592653@gmail.com>
2024-01-25 15:00:47 +0900

    2024/01/25 15:00

---
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -0,0 +1,56 @@
+package com.techacademy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Calendar;//課題用追加
+
+@RestController
+public class KadaiFirstController {
+
+    @GetMapping("/dayofweek/{val1}")
+    public String dispDayOfWeek(@PathVariable String val1) {
+        String date = val1;
+        String nen = date.substring(0,4);
+        String tsuki = date.substring(4,6);
+        String nichi = date.substring(6,8);
+        int year =  Integer.parseInt(nen);
+        int month =  Integer.parseInt(tsuki) -1;
+        int day =  Integer.parseInt(nichi);
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month, day);
+        String weekDay[] = {"Saturday","Sunday","Monday","Tuesday","Wednesday","Tursday","Friday"};
+        return weekDay[cal.get(Calendar.DAY_OF_WEEK)];
+        }
+
+    @GetMapping("/plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+        int resPlus = 0;
+        resPlus = val1 + val2;
+        return "実行結果：" + resPlus;
+    }
+
+    @GetMapping("/minus/{val1}/{val2}")
+    public String calcMinus(@PathVariable int val1, @PathVariable int val2) {
+        int resMinus = 0;
+        resMinus = val1 - val2;
+        return "実行結果：" + resMinus;
+    }
+
+    @GetMapping("/times/{val1}/{val2}")
+    public String calcTimes(@PathVariable int val1, @PathVariable int val2) {
+        int resTimes = 0;
+        resTimes = val1 * val2;
+        return "実行結果：" + resTimes;
+    }
+
+    @GetMapping("/divide/{val1}/{val2}")
+    public String calcDvide(@PathVariable int val1, @PathVariable int val2) {
+        int resDivide = 0;
+        resDivide = val1 / val2;
+        return "実行結果：" + resDivide;
+    }
+
+
+
+}
